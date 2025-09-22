2854196b35f28612557fd4e8fc9cedaf0ac38696
ratetanginamo <168286942+ratetanginamo@users.noreply.github.com>
2025-09-22 12:00:10 +0800

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'pages/login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await Hive.initFlutter();
+  await Hive.openBox('suggestions');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Suggestion App',
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
